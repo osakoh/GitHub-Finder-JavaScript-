b400984297558952bb0f740b4fba25694c4ce3a5
osakoh <macmive@gmail.com>
2021-04-13 17:31:54 +0100

    final commit

---
+++ js/github.js
@@ -0,0 +1,18 @@
+class GitHub {
+    constructor() {
+        this.client_id = '';
+        this.client_secret = '';
+    }
+
+    // get user method
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile: profile
+        }
+    }
+
+}
